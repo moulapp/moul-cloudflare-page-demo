c8a154b3391372dda690a8535e5210da0122c722
Phearak S. Tha <t.sophearak@gmail.com>
2022-04-17 21:28:07 +0700

    Update to latest

--- .moul/app/entry.client.tsx
+++
@@ -1,6 +0,0 @@
-import { hydrate } from 'react-dom'
-import { RemixBrowser } from '@remix-run/react'
-import lazySizes from 'lazysizes'
-lazySizes.cfg.lazyClass = 'lazy'
-
-hydrate(<RemixBrowser />, document)
