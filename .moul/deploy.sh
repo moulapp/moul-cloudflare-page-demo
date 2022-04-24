070c2260feb08e25bf19dcb8e56b5a7d6efced27
Phearak S. Tha <t.sophearak@gmail.com>
2022-04-24 14:45:41 +0700

    Bump version

--- .moul/deploy.sh
+++ .moul/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -xe
-export version='4.0.0-beta.15'
+export version='4.0.0-beta.16'
 curl -LJO "https://github.com/moulapp/moul/releases/download/v${version}/moul_${version}_Linux_x86_64.tar.gz"
 tar -xzf "moul_${version}_Linux_x86_64.tar.gz" -C ./
 ./moul deploy
